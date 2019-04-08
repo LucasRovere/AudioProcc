5d736f8441528153dbbaa6469b0f61cb4187c494
Lucas Rovere <thelucasrovere@gmail.com>
2019-04-08 14:22:29 -0300

    Handling cmd

--- VsGod.py
+++ VsGod.py
@@ -1,1 +1,40 @@
- 
+
+import os
+
+command = ''
+inputs = []
+
+def main():
+	global command, inputs
+
+	OnOpen()
+	quit = False
+
+	while command != 'quit':
+		GetCmd()
+		HandleCmd()
+
+def HandleCmd():
+	if command
+
+def GetCmd():
+	global command, inputs
+
+	inputString = input("VsGod$ ")
+
+	split = inputString.split('>')
+	print(split[0])
+
+	if len(split) == 1:
+		command = split[0].strip()
+	else:
+		command = split[1].strip()
+		inputs.clear()
+		for s in split[0].split('+'):
+			inputs.append(s.strip())
+
+def OnOpen():
+	os.system("clear")
+		
+if __name__ == "__main__":
+	main() 
